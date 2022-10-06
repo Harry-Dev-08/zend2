00d413d2a3aaaefd664b76b2d2390c4e1ee2133d
Hitesh-Dev-08 <sergio.dev08@gmail.com>
2022-10-06 18:49:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllFormsComponent } from './components/all-forms/all-forms.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AllFormsComponent
   ],
   imports: [
     BrowserModule,
